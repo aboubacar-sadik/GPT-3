fb128641ed4ec82d47252e1eda76a9de00ac9d40
Aboubacar Sadik <boube.sadik@gmail.com>
2023-07-10 04:15:03 +0100

    #

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
+import { inject } from '@vercel/analytics';
+
+inject();
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
-import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
-		<Analytics />
 	</React.StrictMode>
 );
